fcb8d5636b3231782ca32305577ecab7ffc9289c
christopherle07 <chriscocemail12@gmail.com>
2026-04-24 00:05:22 -0400

    Fix for production
    -update vite.config.js to output the build directly to public_html/build/

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
     plugins: [
         laravel({
@@ -8,4 +10,10 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        outDir: isProd
+            ? '/home/eyedeane/domains/pelicon.app/public_html/build'
+            : 'public/build',
+        emptyOutDir: true,
+    },
 });
